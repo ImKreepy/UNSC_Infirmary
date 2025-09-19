127e0b1da03170f5e45630ec5e3ffb23515c675e
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-19 14:39:31 -0400

    Did things

--- addons/impalement/ACE_Medical_Injuries.hpp
+++ addons/impalement/ACE_Medical_Injuries.hpp
@@ -1,20 +1,24 @@
 // Impalements will tend to be medium or large
 #define IMPALEMENT(type) class type##Wound {    \
     weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};   \
-    sizeMultiplier = 0.8;   \
+    sizeMultiplier = 0.6;   \
 }
 #define THRESHOLDS {20, 10}, {4.5, 2}, {3, 1}, {0, 1}
 
 class ACE_Medical_Injuries {
     class wounds {
-        class ImpalementWound {
+        class BlamiteWound {
             bleeding = 0.05;
             pain = 0.8;
             causeLimping = 1;
             causeFracture = 1;
         };
-        class BlamiteWound: ImpalementWound {};
-        class SpikeWound: ImpalementWound {};
+        class SpikeWound {
+            bleeding = 0.05;
+            pain = 0.8;
+            causeLimping = 1;
+            causeFracture = 1;
+        };
     };
     class damageTypes {
         class woundHandlers;
@@ -24,26 +28,9 @@ class ACE_Medical_Injuries {
             class PunctureWound {
                 weighting[] = {{1, 1}};
                 sizeMultiplier = 0.6;
-                bleedingMultiplier = 0.9;
-                painMultiplier = 0.5;
+                bleedingMultiplier = 12;
+                painMultiplier = 2;
             };
-        };
-        class Impalement {
-            thresholds[] = {THRESHOLDS};
-            selectionSpecific = 1;
-            class woundHandlers: woundHandlers {
-                GVAR(armorPenetration) = QFUNC(woundsHandlerArmorPenetration);
-            };
-
-            class Avulsion {
-                weighting[] = {{0.6, 1}, {0.35, 0}};
-            };
-            class Contusion {
-                weighting[] = {{0.35, 0}, {0.35, 1}};
-                sizeMultiplier = 3.2;
-                painMultiplier = 2.2;
-            };
-            IMPALEMENT(Impalement);
         };
         class Blamite {
             thresholds[] = {THRESHOLDS};
@@ -54,13 +41,17 @@ class ACE_Medical_Injuries {
 
             class Avulsion {
                 weighting[] = {{0.6, 1}, {0.35, 0}};
+                sizeMultiplier = 0.6;
             };
             class Contusion {
                 weighting[] = {{0.35, 0}, {0.35, 1}};
                 sizeMultiplier = 3.2;
                 painMultiplier = 2.2;
             };
-            IMPALEMENT(Blamite);
+            class BlamiteWound {
+                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
+                sizeMultiplier = 0.5;
+            };
         };
         class Spike {
             thresholds[] = {THRESHOLDS};
@@ -71,13 +62,17 @@ class ACE_Medical_Injuries {
 
             class Avulsion {
                 weighting[] = {{0.6, 1}, {0.35, 0}};
+                sizeMultiplier = 0.6;
             };
             class Contusion {
                 weighting[] = {{0.35, 0}, {0.35, 1}};
                 sizeMultiplier = 3.2;
                 painMultiplier = 2.2;
             };
-            IMPALEMENT(Spike);
+            class SpikeWound {
+                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
+                sizeMultiplier = 0.6;
+            };
         };
         class BlamiteExplosive {
             thresholds[] = {{20, 15}, {8, 7}, {2, 3}, {1.2, 2}, {0.4, 1}, {0,0}};
@@ -93,7 +88,10 @@ class ACE_Medical_Injuries {
                 sizeMultiplier = 2;
                 painMultiplier = 0.9;
             };
-            IMPALEMENT(Blamite);
+            class BlamiteWound {
+                weighting[] = {{0.9, 0}, {0.7, 1}, {0.35, 0}};
+                sizeMultiplier = 0.6;
+            };
         };
         class SpikeGrenade {
             thresholds[] = {{20, 10}, {10, 5}, {4, 3}, {1.5, 2}, {0.8, 2}, {0.3, 1}, {0, 0}};
@@ -109,7 +107,10 @@ class ACE_Medical_Injuries {
                 sizeMultiplier = 2;
                 painMultiplier = 0.9;
             };
-            IMPALEMENT(Spike);
+            class SpikeWound {
+                weighting[] = {{0.9, 0}, {0.7, 1}, {0.35, 0}};
+                sizeMultiplier = 0.6;
+            };
         };
     };
 };
