62b9c66af85f6f9bbe8b87aa38d171075519ef81
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-24 02:00:35 -0400

    Class names

--- addons/treatment/config.cpp
+++ addons/treatment/config.cpp
@@ -7,8 +7,16 @@ class CfgPatches {
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"unsci_main","ace_medical_treatment"};
-        units[] = {};
-        weapons[] = {};
+        units[] = {
+            "UNSCI_Biofoam_Item",
+            "UNSCI_Medigel_Item",
+            "UNSCI_Medkit_Item"
+        };
+        weapons[] = {
+            "UNSCI_Biofoam",
+            "UNSCI_Medigel",
+            "UNSCI_Medkit"
+        };
         VERSION_CONFIG;
 
          
@@ -18,3 +26,5 @@ class CfgPatches {
 #include "CfgEventHandlers.hpp"
 #include "ACE_Medical_Treatment.hpp"
 #include "ACE_Medical_Treatment_Actions.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
