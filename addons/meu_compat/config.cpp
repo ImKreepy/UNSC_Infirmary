126e91657a220e6e17b277be1cfed685d25ca51c
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-25 13:34:44 -0400

    Added dependancies

--- addons/meu_compat/config.cpp
+++ addons/meu_compat/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         authors[] = {"Im Kreepy"};
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"unsci_main"};
+        requiredAddons[] = {"unsci_main","1st_MEU_patch_ace","1st_MEU_patch_ace_medical"};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
@@ -18,3 +18,5 @@ class CfgPatches {
 #include "ACE_Medical_Injuries.hpp"
 #include "ACE_Medical_Treatment.hpp"
 #include "ACE_Medical_Treatment_Actions.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
