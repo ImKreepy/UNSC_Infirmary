c1e80ca4c0f9877381c1e1e42c4900ad0c1da3c0
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-25 15:09:38 -0400

    More ammo

--- addons/damage/CfgAmmo.hpp
+++ addons/damage/CfgAmmo.hpp
@@ -1,8 +1,8 @@
 class CfgAmmo {
     class OPTRE_FC_Bolt_Base;
     class OPTRE_M41_Rocket_ATGM2;
-    class OPTRE_B_127x99_Ball;
 	class OPTRE_sticky_explosion_base;
+    class OPTRE_B_127x99_Ball;
     class APERSBoundingMine_Range_Ammo;
     class M_NLAW_AT_F;
     class M_Titan_AA;
@@ -61,16 +61,28 @@ class CfgAmmo {
     class OPTRE_FC_HeavyPlasma_Bolt_AG : ammo_AAA_Gun35mm_AA {
         ACE_damageType = "UNSCI_PlasmaExplosive";
     };
+    class OPTRE_FC_C2_Cannon_Bolt : OPTRE_FC_T51_Repeater_Bolt {
+        ACE_damageType = "UNSCI_PlasmaExplosive";
+    };
     class OPTRE_FC_T33_FuelRod_Veh : Rocket_04_HE_F {
         ACE_damageType = "UNSCI_PlasmaExplosive";
     };
     class OPTRE_FC_T33_FuelRod_VehAA : M_70mm_SAAMI {
         ACE_damageType = "UNSCI_PlasmaExplosive";
     };
+    class OPTRE_FC_T33_FuelRod_VehAA_Ultra : OPTRE_FC_T33_FuelRod_VehAA {
+        ACE_damageType = "UNSCI_PlasmaExplosive";
+    };
     class OPTRE_FC_T33_FuelRod_VehATGM : OPTRE_M41_Rocket_ATGM2 {
         ACE_damageType = "UNSCI_PlasmaExplosive";
     };
+    class OPTRE_FC_T33_FuelRod_VehATGM_Ultra : OPTRE_FC_T33_FuelRod_VehATGM {
+        ACE_damageType = "UNSCI_PlasmaExplosive";
+    };
     class OPTRE_FC_T46_Rod : OPTRE_B_127x99_Ball {
+        ACE_damageType = "UNSCI_PlasmaExplosive";
+    };
+    class OPTRE_FC_T46_Rod_Red : OPTRE_FC_T46_Rod {
         ACE_damageType = "UNSCI_PlasmaExplosive";
     };
 
@@ -78,14 +90,23 @@ class CfgAmmo {
 	class OPTRE_FC_Needler_ammo : OPTRE_FC_Bolt_Base {
         ACE_damageType = "UNSCI_Blamite";
     };
+    class OPTRE_FC_Blamite_Shards : OPTRE_FC_Needler_ammo {
+        ACE_damageType = "UNSCI_Blamite";
+    };
 
 	class OPTRE_FC_NeedleMine_Dispenser_Needle_Mine : APERSBoundingMine_Range_Ammo {
         ACE_damageType = "UNSCI_BlamiteExplosive";
     };
-    class OPTRE_AA_Big_Needler_ammo : M_Air_AA {
+    class OPTRE_FC_HeavyNeedle : OPTRE_FC_HeavyPlasma_Bolt_AG {
         ACE_damageType = "UNSCI_BlamiteExplosive";
     };
-    class OPTRE_FC_HeavyNeedle : OPTRE_FC_HeavyPlasma_Bolt_AG {
+    class OPTRE_FC_T33_FuelRod_VehATGM_Needler : OPTRE_FC_T33_FuelRod_VehATGM {
+        ACE_damageType = "UNSCI_BlamiteExplosive";
+    };
+    class OPTRE_FC_T46_Rod_Needler : OPTRE_FC_T46_Rod {
+        ACE_damageType = "UNSCI_BlamiteExplosive";
+    };
+    class OPTRE_AA_Big_Needler_ammo : M_Air_AA {
         ACE_damageType = "UNSCI_BlamiteExplosive";
     };
     class OPTRE_AA_SAM_Needler_ammo : ammo_Missile_LongRangeAABase {
