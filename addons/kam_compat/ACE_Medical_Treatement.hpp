7b568f5d592a5a2a37d023a9000788e0d299acd9
Im Kreepy <jkoscinski3@hotmail.com>
2025-10-07 23:51:31 -0400

    Added KILL_WOUND_TYPES to BloodClot bandages

---
+++ addons/kam_compat/ACE_Medical_Treatement.hpp
@@ -0,0 +1,14 @@
+class ACE_Medical_Treatment {
+    class Bandaging {
+        class FieldDressing;
+        class BloodClotMinor: FieldDressing {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotMedium: FieldDressing {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotLarge: FieldDressing {
+            KILL_WOUND_TYPES;
+        };
+    };
+};
