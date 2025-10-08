cc9d5e3f1f58ac86c7fef5453044d7477f5db5b1
Im Kreepy <jkoscinski3@hotmail.com>
2025-10-08 00:08:38 -0400

    Fixed

--- addons/kam_compat/ACE_Medical_Treatment.hpp
+++ addons/kam_compat/ACE_Medical_Treatment.hpp
@@ -10,5 +10,25 @@ class ACE_Medical_Treatment {
         class BloodClotLarge: FieldDressing {
             KILL_WOUND_TYPES;
         };
+
+        class BloodClotMinorTXA: BloodClotMinor {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotMediumTXA: BloodClotMedium {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotLargeTXA: BloodClotLarge {
+            KILL_WOUND_TYPES;
+        };
+
+        class BloodClotMinorEACA: BloodClotMinor {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotMediumEACA: BloodClotMedium {
+            KILL_WOUND_TYPES;
+        };
+        class BloodClotLargeEACA: BloodClotLarge {
+            KILL_WOUND_TYPES;
+        };
     };
 };
--- addons/kam_compat/config.cpp
+++ addons/kam_compat/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         authors[] = {"Im Kreepy"};
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"unsci_main","kat_breathing","kat_pharma"};
+        requiredAddons[] = {"unsci_main","kat_breathing","kat_pharma","kat_surgery"};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
