dfe8c8141e3d421df5a2036d60f2b781efe5a1c7
Im Kreepy <jkoscinski3@hotmail.com>
2025-10-20 17:08:18 -0400

    Sourced directly for AOR's Ammo file over creating a new one.

--- addons/aor_compat/config.cpp
+++ addons/aor_compat/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         authors[] = {"Im Kreepy"};
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"unsci_main","unsci_damage","unsci_treatment","aor_medical",};
+        requiredAddons[] = {"unsci_main","unsci_damage","unsci_treatment","aor_medical_ace",};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
@@ -17,3 +17,4 @@ class CfgPatches {
 
 #include "ACE_Medical_Injuries.hpp"
 #include "ACE_Medical_Treatment_Actions.hpp"
+#include "\z\aor\addons\medical_ace\CfgAmmo.hpp"
