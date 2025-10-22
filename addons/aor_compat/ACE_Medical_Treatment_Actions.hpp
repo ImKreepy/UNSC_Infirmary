b0a370766bd859f67cd3a3b30def23e0d4868c93
Im Kreepy <jkoscinski3@hotmail.com>
2025-10-22 14:39:44 -0400

    Added items

--- addons/aor_compat/ACE_Medical_Treatment_Actions.hpp
+++ addons/aor_compat/ACE_Medical_Treatment_Actions.hpp
@@ -9,12 +9,12 @@ class ACE_Medical_Treatment_Actions {
     delete UNSCI_Medkit;
 
     class AOR_Biofoam: BasicBandage {
-        items[] = {"UNSCI_Biofoam"};
+        items[] = {"UNSCI_Biofoam","AOR_Biofoam"};
     };
     class AOR_Medigel: AOR_Biofoam {
-        items[] = {"UNSCI_MediGel_Canister","UNSCI_MediGel_40ml"};
+        items[] = {"UNSCI_MediGel_Canister","UNSCI_MediGel_40ml","AOR_Medigel"};
     };
     class AOR_Medkit: AOR_Biofoam {
-        items[] = {"UNSCI_Medkit"};
+        items[] = {"UNSCI_Medkit","AOR_Medkit"};
     };
 };
