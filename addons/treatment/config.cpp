ce29d50ec837d84f869390a4943929dcc95005b4
Im Kreepy <jkoscinski3@hotmail.com>
2025-10-22 14:30:57 -0400

    Needed dependancies

--- addons/treatment/config.cpp
+++ addons/treatment/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         authors[] = {"Im Kreepy"};
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"unsci_main","ace_medical_treatment"};
+        requiredAddons[] = {"unsci_main","unsci_items","unsci_damage","ace_medical_treatment"};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
