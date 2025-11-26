7f0c2f17f80ceb70e5d6e826a75953a86eca7185
Im Kreepy <jkoscinski3@hotmail.com>
2025-11-26 01:19:07 -0500

    Added build for versioning

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,3 +1,4 @@
 #define MAJOR 1// Major Update, used for massive changes
 #define MINOR 4// Minor Update, used for new features and improvements
-#define PATCH 0// Patch level, used for bug fixes and minor improvements
+#define PATCH 1// Patch level, used for bug fixes and minor improvements
+#define BUILD 7// Build number, adds all versions together for a unique build number
