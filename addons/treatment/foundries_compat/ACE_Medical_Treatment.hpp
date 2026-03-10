d2df472aac88114098c48ea747ca40908aee3d9b
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-10 14:26:05 -0400

    Foundries compat

---
+++ addons/treatment/foundries_compat/ACE_Medical_Treatment.hpp
@@ -0,0 +1,8 @@
+class ACE_Medical_Treatment {
+    class Bandaging {
+        class ElasticBandage;
+        class DMNS_Biofoam: ElasticBandage {
+            UNSCI_PLASMA_BIOFOAM;
+        };
+    };
+};
