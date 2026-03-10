289cb2c2b3fc8a17ca7a4eab63eb40e8aa582030
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-10 13:47:22 -0400

    Turned Plasma burns into a macro

--- addons/treatment/ACE_Medical_Treatment.hpp
+++ addons/treatment/ACE_Medical_Treatment.hpp
@@ -41,37 +41,7 @@ class ACE_Medical_Treatment {
         // Biofoam is high treatment, lower reopen chance, longer reopening delay
         class UNSCI_Biofoam : FieldDressing {
             // ----- UNSCI Wound Treatments -----
-            class UNSCI_PlasmaBurn {
-                effectiveness = 1;
-                reopeningChance = 0.2;
-                reopeningMinDelay = 500;
-                reopeningMaxDelay = 1000;
-            };
-            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn {
-                effectiveness = 1;
-            };
-            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {
-                effectiveness = 0.8;
-            };
-            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn {
-                effectiveness = 0.65;
-            };
-
-            class UNSCI_PlasmaAvulsion {
-                effectiveness = 5;
-                reopeningChance = 0.3;
-                reopeningMinDelay = 800;
-                reopeningMaxDelay = 1500;
-            };
-            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion {
-                effectiveness = 5;
-            };
-            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {
-                effectiveness = 4;
-            };
-            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion {
-                effectiveness = 3;
-            };
+            UNSCI_PLASMA_BIOFOAM;
 
             // ----- ACE Wound Treatments -----
             class Abrasion {
@@ -210,37 +180,7 @@ class ACE_Medical_Treatment {
         // MediGel is average treatment, high reopen chance, shorter reopening delay
         class UNSCI_MediGel : FieldDressing {
             // ----- UNSCI Wound Treatments -----
-            class UNSCI_PlasmaBurn {
-                effectiveness = 5;
-                reopeningChance = 0.6;
-                reopeningMinDelay = 80;
-                reopeningMaxDelay = 150;
-            };
-            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn {
-                effectiveness = 5;
-            };
-            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {
-                effectiveness = 4;
-            };
-            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn {
-                effectiveness = 3;
-            };
-
-            class UNSCI_PlasmaAvulsion {
-                effectiveness = 0.4;
-                reopeningChance = 1;
-                reopeningMinDelay = 50;
-                reopeningMaxDelay = 100;
-            };
-            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion {
-                effectiveness = 0.4;
-            };
-            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {
-                effectiveness = 0.25;
-            };
-            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion {
-                effectiveness = 0.1;
-            };
+            UNSCI_PLASMA_MEDIGEL;
 
             // ----- ACE Wound Treatments -----
             class Abrasion {
@@ -379,37 +319,7 @@ class ACE_Medical_Treatment {
 
         class UNSCI_Medkit : FieldDressing {
             // ----- UNSCI Wound Treatments -----
-            class UNSCI_PlasmaBurn {
-                effectiveness = 10;
-                reopeningChance = 0.3;
-                reopeningMinDelay = 50;
-                reopeningMaxDelay = 100;
-            };
-            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn {
-                effectiveness = 10;
-            };
-            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {
-                effectiveness = 10;
-            };
-            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn {
-                effectiveness = 10;
-            };
-
-            class UNSCI_PlasmaAvulsion {
-                effectiveness = 10;
-                reopeningChance = 0.3;
-                reopeningMinDelay = 80;
-                reopeningMaxDelay = 150;
-            };
-            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion {
-                effectiveness = 10;
-            };
-            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {
-                effectiveness = 10;
-            };
-            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion {
-                effectiveness = 10;
-            };
+            UNSCI_PLASMA_MEDKIT;
 
             // ----- ACE Wound Treatments -----
             class Abrasion {
--- addons/treatment/script_component.hpp
+++ addons/treatment/script_component.hpp
@@ -4,3 +4,96 @@
 #include "\ik\unsci\addons\main\script_mod.hpp"
 
 #include "\ik\unsci\addons\main\script_macros.hpp"
+
+#define UNSCI_PLASMA_BIOFOAM class UNSCI_PlasmaBurn {   \
+                effectiveness = 1;  \
+                reopeningChance = 0.2;  \
+                reopeningMinDelay = 500;    \
+                reopeningMaxDelay = 1000;   \
+            };  \
+            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn { \
+                effectiveness = 1;  \
+            };  \
+            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {    \
+                effectiveness = 0.8;    \
+            };  \
+            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn { \
+                effectiveness = 0.65;   \
+            };  \
+            class UNSCI_PlasmaAvulsion {    \
+                effectiveness = 5;  \
+                reopeningChance = 0.3;  \
+                reopeningMinDelay = 800;    \
+                reopeningMaxDelay = 1500;   \
+            };  \
+            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion { \
+                effectiveness = 5;  \
+            };  \
+            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {    \
+                effectiveness = 4;  \
+            };  \
+            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion { \
+                effectiveness = 3;  \
+            }
+
+#define UNSCI_PLASMA_MEDIGEL class UNSCI_PlasmaBurn {   \
+                effectiveness = 5;  \
+                reopeningChance = 0.6;  \
+                reopeningMinDelay = 80;    \
+                reopeningMaxDelay = 150;   \
+            };  \
+            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn { \
+                effectiveness = 5;  \
+            };  \
+            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {    \
+                effectiveness = 4;    \
+            };  \
+            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn { \
+                effectiveness = 3;  \
+            };  \
+            class UNSCI_PlasmaAvulsion {    \
+                effectiveness = 0.4;    \
+                reopeningChance = 1;    \
+                reopeningMinDelay = 50;    \
+                reopeningMaxDelay = 100;   \
+            };  \
+            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion {   \
+                effectiveness = 0.4;    \
+            };  \
+            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {    \
+                effectiveness = 0.25;   \
+            };  \
+            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion { \
+                effectiveness = 0.1;    \
+            }
+
+#define UNSCI_PLASMA_MEDKIT class UNSCI_PlasmaBurn {   \
+                effectiveness = 10;  \
+                reopeningChance = 0.3;  \
+                reopeningMinDelay = 50;    \
+                reopeningMaxDelay = 100;   \
+            };  \
+            class UNSCI_PlasmaBurnMinor: UNSCI_PlasmaBurn { \
+                effectiveness = 10;  \
+            };  \
+            class UNSCI_PlasmaBurnMedium: UNSCI_PlasmaBurn {    \
+                effectiveness = 10;    \
+            };  \
+            class UNSCI_PlasmaBurnLarge: UNSCI_PlasmaBurn { \
+                effectiveness = 10;  \
+            };  \
+            class UNSCI_PlasmaAvulsion {    \
+                effectiveness = 10;  \
+                reopeningChance = 0.3;  \
+                reopeningMinDelay = 80;    \
+                reopeningMaxDelay = 150;   \
+            };  \
+            class UNSCI_PlasmaAvulsionMinor: UNSCI_PlasmaAvulsion {   \
+                effectiveness = 10;  \
+            };  \
+            class UNSCI_PlasmaAvulsionMedium: UNSCI_PlasmaAvulsion {    \
+                effectiveness = 10;    \
+            };  \
+            class UNSCI_PlasmaAvulsionLarge: UNSCI_PlasmaAvulsion { \
+                effectiveness = 10;    \
+            }
