d3c64ea0d5503cc7550324308d2341f1bc91e645
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-10 14:03:30 -0400

    #

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 1// Major Update, used for massive changes
-#define MINOR 4// Minor Update, used for new features and improvements
-#define PATCH 3// Patch level, used for bug fixes and minor improvements
-#define BUILD 9// Build number, adds all versions together for a unique build number
+#define MINOR 5// Minor Update, used for new features and improvements
+#define PATCH 0// Patch level, used for bug fixes and minor improvements
+#define BUILD 10// Build number, adds all versions together for a unique build number
