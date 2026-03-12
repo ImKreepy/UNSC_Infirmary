e56f00c86343cfcbcba8b84854d284eb72658de2
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-12 14:38:52 -0400

    Added AOR macro definitions for medical compatibility

--- addons/treatment/aor_compat/script_component.hpp
+++ addons/treatment/aor_compat/script_component.hpp
@@ -2,3 +2,10 @@
 #define SUBCOMPONENT_BEAUTIFIED Age of Reclamation Compatability
 
 #include "..\script_component.hpp"
+
+#define AOR_PREFIX aor
+#define AORGVAR(var) TRIPLES(AOR_PREFIX,medical_ace,var)
+#define AORFUNC(var) TRIPLES(AOR_PREFIX,medical_ace,DOUBLES(fnc,var))
+#define AORQGVAR(var) QUOTE(AORGVAR(var))
+#define AORQFUNC(var) QUOTE(AORFUNC(var))
+#define AORCSTRING(var) QUOTE(TRIPLES($STR,DOUBLES(AOR_PREFIX,medical_ace),var))
