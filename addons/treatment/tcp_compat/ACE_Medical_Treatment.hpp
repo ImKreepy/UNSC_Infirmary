9d97a1544496b74a7122ce0eb69a67c55278a635
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-15 16:22:39 -0400

    Add UNSCI Plasma Biofoam and MediGel treaments to TCP Medical Treaments

--- addons/treatment/tcp_compat/ACE_Medical_Treatment.hpp
+++ addons/treatment/tcp_compat/ACE_Medical_Treatment.hpp
@@ -1,3 +1,11 @@
 class ACE_Medical_Treatment {
-
+    class Bandaging {
+        class FieldDressing;
+        class TCP_ACE_BiomedicalFoam: FieldDressing {
+            UNSCI_PLASMA_BIOFOAM;
+        };
+        class TCP_ACE_Medigel: FieldDressing {
+            UNSCI_PLASMA_MEDIGEL;
+        };
+    };
 };
