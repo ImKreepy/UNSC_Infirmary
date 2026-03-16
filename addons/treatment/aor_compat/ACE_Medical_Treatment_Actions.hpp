64661ac0728853ed76e7eaa8036970fd40cb189a
Im Kreepy <jkoscinski3@hotmail.com>
2026-03-16 13:30:48 -0400

    Renamed display names and progress messages for AOR medical items to mimic UNSCI items

--- addons/treatment/aor_compat/ACE_Medical_Treatment_Actions.hpp
+++ addons/treatment/aor_compat/ACE_Medical_Treatment_Actions.hpp
@@ -8,12 +8,18 @@ class ACE_Medical_Treatment_Actions {
     delete UNSCI_Medkit;
 
     class AOR_Biofoam: BasicBandage {
+        displayName = "Biomedical Foam";
+        displayNameProgress = "Injecting Biomedical Foam...";
         items[] = {"UNSCI_Biofoam","AOR_Biofoam"};
     };
     class AOR_Medigel: AOR_Biofoam {
+        displayName = "MediGel";
+        displayNameProgress = "Applying MediGel...";
         items[] = {"UNSCI_MediGel_Canister","UNSCI_MediGel_40ml","AOR_Medigel"};
     };
     class AOR_Medkit: AOR_Biofoam {
+        displayName = "Medical Kit";
+        displayNameProgress = "Using Medical Kit...";
         items[] = {"UNSCI_Medkit","AOR_Medkit"};
     };
 };
