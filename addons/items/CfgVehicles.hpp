df78a3716db240e586db964b701cd203608eb8e4
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-11 14:30:44 -0400

    Remove TransportMagazines from various Biofoam and MediGel items

--- addons/items/CfgVehicles.hpp
+++ addons/items/CfgVehicles.hpp
@@ -21,11 +21,13 @@ class CfgVehicles
         vehicleClass = "Props";
         editorCategory = "UNSCI_EdCat_Props";
         model = "\OPTRE_Weapons\items\Biofoam.p3d";
+        delete TransportMagazines;
     };
 
     class UNSCI_MediGel_Canister_Item: UNSCI_Biofoam_Item {
         displayName = "[UNSCI] MediGel Canister";
         class TransportMagazines {
+            delete _xx_UNSCI_Biofoam;
             class _xx_UNSCI_Medigel_Canister {
                 magazine = "UNSCI_Medigel_Canister";
                 count = 1;
@@ -36,6 +38,7 @@ class CfgVehicles
         vehicleClass = "Props";
         editorCategory = "UNSCI_EdCat_Props";
         model = "\OPTRE_Weapons\items\Medigel.p3d";
+        delete TransportMagazines;
     };
 
     class UNSCI_MediGel_40ml_Item: UNSCI_MediGel_Canister_Item {
@@ -51,6 +54,7 @@ class CfgVehicles
         vehicleClass = "Props";
         editorCategory = "UNSCI_EdCat_Props";
         model = "\ik\unsci\addons\items\data\unsci_medigel_tube.p3d";
+        delete TransportMagazines;
     };
 
     class UNSCI_Medkit_Item: UNSCI_Biofoam_Item {
@@ -59,6 +63,7 @@ class CfgVehicles
         displayName = "[UNSCI] Medical Kit";
         author = AUTHOR;
         class TransportItems {
+            delete _xx_UNSCI_Biofoam;
             class _xx_UNSCI_Medkit {
                 name = "UNSCI_Medkit";
                 count = 1;
@@ -69,6 +74,7 @@ class CfgVehicles
         vehicleClass = "Props";
         editorCategory = "UNSCI_EdCat_Props";
         model = "\OPTRE_Weapons\items\MedKit.p3d";
+        delete TransportMagazines;
     };
 
     class UNSCI_MedkitPlasmaIV_Item: Item_Base_F {
@@ -104,11 +110,13 @@ class CfgVehicles
         vehicleClass = "Props";
         editorCategory = "UNSCI_EdCat_Props";
         model = QPATHTOF(data\unsci_injector.p3d);
+        delete TransportMagazines;
     };
 
     class UNSCI_Injector_Morphine_Item: UNSCI_Injector_Stimulant_Item {
         displayName = "[UNSCI] Morphine Autoinjector";
         class TransportItems {
+            delete _xx_UNSCI_Injector_Stimulant;
             class _xx_UNSCI_Injector_Morphine {
                 name = "UNSCI_Injector_Morphine";
                 count = 1;
@@ -119,11 +127,13 @@ class CfgVehicles
         displayName = "[UNSCI] Morphine Autoinjector";
         hiddenSelections[] = {"camo","camo1","camo2"};
         hiddenSelectionsTextures[] = {QPATHTOF(data\injector\unsci_injector_base_co.paa),QPATHTOF(data\injector\unsci_injector_Cap_co.paa),QPATHTOF(data\injector\unsci_injector_morphine_co.paa)};
+        delete TransportMagazines;
     };
 
     class UNSCI_Injector_Epinephrine_Item: UNSCI_Injector_Stimulant_Item {
         displayName = "[UNSCI] Epinephrine Autoinjector";
         class TransportItems {
+            delete _xx_UNSCI_Injector_Stimulant;
             class _xx_UNSCI_Injector_Epinephrine {
                 name = "UNSCI_Injector_Epinephrine";
                 count = 1;
@@ -134,6 +144,7 @@ class CfgVehicles
         displayName = "[UNSCI] Epinephrine Autoinjector";
         hiddenSelections[] = {"camo","camo1","camo2"};
         hiddenSelectionsTextures[] = {QPATHTOF(data\injector\unsci_injector_base_co.paa),QPATHTOF(data\injector\unsci_injector_Cap_co.paa),QPATHTOF(data\injector\unsci_injector_epinephrine_co.paa)};
+        delete TransportMagazines;
     };
 
     // ----- Litter -----
