49433d0183a77b01bd517069dc41fd9f8cc5697c
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-14 00:24:40 -0400

    Update mod metadata with GitHub link and enhance overview description

--- mod.cpp
+++ mod.cpp
@@ -3,8 +3,8 @@ name = "UNSC Infirmary";
 author = "Im Kreepy";
 description = "UNSC Infirmary brings ACE Advanced Medical to the Halo enthusiast";
 
-//action = "https://discord.gg/FqY9cNzB5n";
-//actionName = "Join Jaeger Company";
+action = "https://github.com/ImKreepy/UNSC_Infirmary";
+actionName = "GitHub";
 
 tooltip = "UNSC Infirmary";
 tooltipOwned = "UNSC Infirmary Owned";
@@ -14,7 +14,7 @@ logo = "\ik\unsci\addons\main\logos\unsci_logo_ca.paa"; // Image displayed on th
 logoOver = "\ik\unsci\addons\main\logos\unsci_logoover_ca.paa"; // Image displayed when the mouse hovers over the image on the main menu (128x)
 logoSmall = "\ik\unsci\addons\main\logos\unsci_logo_small_ca.paa"; // Image displayed in Arsenal or 3DEN (32x)
 
-overview = "This mod adds various medical items, injuries, and treatments that portray the universe of Halo";
+overview = "UNSC Infirmary is a mod for ACE 3 that expands medical injuries, treatments, and items that represent the world of Halo.";
 overviewText = "UNSC Infirmary";
 overviewPicture = "\ik\unsci\addons\main\logos\unsci_logo_ca.paa";
 overviewFootnote = "\ik\unsci\addons\main\logos\unsci_logo_ca.paa";
