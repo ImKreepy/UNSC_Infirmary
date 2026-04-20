b6d09405513fd9d406fd33eded9a68c5505954c8
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-20 01:09:31 -0400

    Update path for Torso_Biofoam texture to include 'wounds' directory

--- addons/kam_compat/gui.hpp
+++ addons/kam_compat/gui.hpp
@@ -5,7 +5,7 @@ class ACEGVAR(medical_gui,BodyImage): RscControlsGroupNoScrollbars {
         class Torso_ChestSeal;
         class Torso_Biofoam: Torso_ChestSeal {
             idc = IDC_BODY_TORSO_BIOFOAM;
-            text = QPATHTOEF(gui,data\body_image\unsci_torso_biofoam.paa);
+            text = QPATHTOEF(gui,data\body_image\wounds\unsci_torso_biofoam.paa);
         };
     };
 };
