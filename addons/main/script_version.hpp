9e9d36e764790bb5d2f08503ebc6893f56d973b5
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-20 01:15:36 -0400

    Update patch and build numbers in script_version.hpp

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 1// Major Update, used for massive changes
 #define MINOR 7// Minor Update, used for new features and improvements
-#define PATCH 2// Patch level, used for bug fixes and minor improvements
-#define BUILD 17// Build number, adds all versions together for a unique build number
+#define PATCH 3// Patch level, used for bug fixes and minor improvements
+#define BUILD 18// Build number, adds all versions together for a unique build number
