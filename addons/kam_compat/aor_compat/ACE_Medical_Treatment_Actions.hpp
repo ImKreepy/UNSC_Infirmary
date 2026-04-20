0782658069c3f2de0d41d2082e76620a7528368f
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-20 01:43:04 -0400

    Wrong inheritance

--- addons/kam_compat/aor_compat/ACE_Medical_Treatment_Actions.hpp
+++ addons/kam_compat/aor_compat/ACE_Medical_Treatment_Actions.hpp
@@ -1,6 +1,6 @@
 class ACE_Medical_Treatment_Actions {
-    class AOR_Biofoam;
-    class AOR_Medkit: AOR_Biofoam {
+    class BasicBandage;
+    class AOR_Medkit: BasicBandage {
         callbackSuccess = QEFUNC(kam_compat,medkit);
     };
     class UNSCI_Biofoam;
